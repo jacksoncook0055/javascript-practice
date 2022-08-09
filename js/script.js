7af844ba821e0d8a0843d0aee213b25ef62553a5
Jackson Cook <jacksoncook0055@gmai.com>
2022-08-08 20:41:35 -0700

    added js comments and console logs

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//this is a single line comment
+
+/*this is a 
+multi-line commment*/
+
+const myObj ={firstname:"Jackson", lastname:"Cook"};
+console.log(myObj)
+
+const myArr = ["Orange", "Banana", "Mango", "Kiwi"];
+console.log(myArr);
